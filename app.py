7f5f25b87ec2b59694e71240b3a5bbc90d144216
Prashant Kumar <kumarprashant670@gmail.com>
2020-08-05 15:32:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-import json
-import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import precision_score, recall_score
-import pydeck as pdk
-import folium
-import streamlit.components.v1 as components
 import datetime
 import plotly.express as px
 import matplotlib.pyplot as plt
